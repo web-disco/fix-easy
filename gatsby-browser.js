4d789c772a41ebf651d70ec60c037ad544bdfd9b
Joshua Isaac <hello.joshua@hotmail.com>
2021-06-27 11:59:23 -0400

    fix gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import "./src/styles/global.css"
 import React from "react"
 import FacebookChat from "./src/components/facebook-chat/FacebookChat"
 
-exports.wrapPageElement = ({ element, props }) => (
+export const wrapPageElement = ({ element, props }) => (
   <React.Fragment {...props}>
     {element}
     <FacebookChat />
