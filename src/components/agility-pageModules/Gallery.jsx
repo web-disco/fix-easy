064d6f89c2452f704f5ee0b169fb562455462d07
Joshua Isaac <hello.joshua@hotmail.com>
2021-06-29 17:02:09 -0400

    switching to swr

--- src/components/agility-pageModules/Gallery.jsx
+++ src/components/agility-pageModules/Gallery.jsx
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
+import useSWR from "swr"
 import { AgilityImage } from "@agility/gatsby-image-agilitycms"
 import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox"
+import { ImSpinner } from "react-icons/im"
 
 const Gallery = ({ module }) => {
   const { customFields } = module
 
-  // set up gallery
-  const [gallery, setGallery] = useState({
-    media: [],
-  })
+  // fetcher for swr
+  const fetcher = url =>
+    fetch(url, {
+      headers: {
+        Accept: "application/json",
+        Apikey: `${process.env.GATSBY_AGILITY_API_KEY}`,
+      },
+    }).then(res => res.json())
 
-  // fetch gallery on component load
-  useEffect(() => {
-    // get gallery function
-    const getGallery = async () => {
-      const gallery = await fetch(
-        `/api/gallery?id=${customFields.gallery.galleryid}`
-      )
-      setGallery(await gallery.json())
-    }
+  // fetch gallery
+  const { data, error } = useSWR(
+    `/api/gallery?id=${customFields.gallery.galleryid}`,
+    fetcher
+  )
+  // error fetching gallery
+  if (error) {
+    return (
+      <div className="max-w-screen-xl px-4 md:px-8 mx-auto text-center py-20">
+        <p>We're sorry, we could not load gallery...</p>
+      </div>
+    )
+  }
 
-    // call get gallery
-    getGallery()
-  }, [customFields.gallery.galleryid])
+  // // loading gallery
+  if (!data) {
+    return (
+      <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
+        <ImSpinner className="animate-spin text-4xl" />
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-screen-xl px-4 md:px-8 mx-auto my-12">
-      {gallery && gallery.media.length > 0 ? (
-        <SimpleReactLightbox>
-          <SRLWrapper>
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-8">
-              {gallery.media.map((image, index) => (
-                <a href={image.url} key={index}>
-                  <AgilityImage
-                    image={image}
-                    layout="fullWidth"
-                    className="rounded-md"
-                  />
-                </a>
-              ))}
-            </div>
-          </SRLWrapper>
-        </SimpleReactLightbox>
-      ) : (
-        <div className="text-center py-20 text-4xl">
-          The gallery could not be loaded at this time.
-        </div>
-      )}
+      <SimpleReactLightbox>
+        <SRLWrapper>
+          {data.media.map((image, index) => (
+            <a href={image.url} key={index}>
+              <AgilityImage
+                image={image}
+                layout="fullWidth"
+                className="rounded-md"
+              />
+            </a>
+          ))}
+        </SRLWrapper>
+      </SimpleReactLightbox>
     </div>
   )
 }
